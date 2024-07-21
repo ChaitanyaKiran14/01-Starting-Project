b98637705260a1501391a876dbfbcb38842c7fe1
chaitanya <g.chaitanya1419@gmail.com>
2024-07-21 19:30:21 +0530

    used Type

--- src/CourseGoal/index.tsx
+++ src/CourseGoal/index.tsx
@@ -1,9 +1,18 @@
-const CourseGoal = ({title, description} : {
-  title :string, description : string
-}) => {
+//const CourseGoal = ({title, description} : {
+  //title :string, description : string    //this is a bit hard to read so what we do is we define a custom type so that we can use it whenever we want
+//}) => {
+
+
+type CourseProps  = {
+  title : string;
+  description : string;
+
+}
+const CourseGoal = ({title, description}: CourseProps) => {
 
   return (
       <div>
+
          <h1>{title}</h1>
          <p>{description}</p>
       </div>
